90d0d635c9582549c54cbcfabac2782d07129c9f
plastikSun <marcom2k01@gmail.com>
2023-10-28 22:22:15 +0200

    create: type migration & up() function

--- database/migrations/2023_10_28_201316_create_types_table.php
+++ database/migrations/2023_10_28_201316_create_types_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 100);
+            $table->string('label', 30);
             $table->timestamps();
         });
     }
